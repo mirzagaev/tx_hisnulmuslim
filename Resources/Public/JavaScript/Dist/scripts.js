50da0e2c3f86ee9ca5574807d75bde5627557530
Aydin <aydin.mirzagaev@gmx.de>
2025-10-06 13:22:41 +0200

    extension pdate

--- Resources/Public/JavaScript/Dist/scripts.js
+++ Resources/Public/JavaScript/Dist/scripts.js
@@ -1,3 +1,16 @@
+// beim Laden
+if (localStorage.getItem('theme') === 'dark') {
+  document.documentElement.classList.add('dark');
+}
+
+// beim Umschalten
+function toggleDarkMode() {
+  document.documentElement.classList.toggle('dark');
+  localStorage.setItem('theme',
+    document.documentElement.classList.contains('dark') ? 'dark' : 'light'
+  );
+}
+
 $(document).ready(function() {
     $("#search_open").click(function() {
         $("h1").hide();
